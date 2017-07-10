5986bfdfc5d015293c0128bef8e8c4f26c67b211
vivekpowar <vivek.p@mobinexttech.com>
2017-07-10 14:56:57 +0530

    some changes in method and gradle properties

--- app/src/main/java/com/example/vivek/mqttconnection/MainActivity.java
+++ app/src/main/java/com/example/vivek/mqttconnection/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import org.eclipse.kapua.gateway.client.Application;
 import org.eclipse.kapua.gateway.client.Client;
+import org.eclipse.kapua.gateway.client.ErrorHandler;
 import org.eclipse.kapua.gateway.client.MessageHandler;
 import org.eclipse.kapua.gateway.client.Payload;
 import org.eclipse.kapua.gateway.client.Sender;
@@ -26,6 +27,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Optional;
 
 import static org.eclipse.kapua.gateway.client.Credentials.userAndPassword;
 import static org.eclipse.kapua.gateway.client.Errors.ignore;
@@ -51,83 +53,74 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-
-    public void Connect() throws Exception
-    {
-        try (Client client = KuraMqttProfile.newProfile(FuseClient.Builder::new)
-                .accountName("kapua-sys")
-                .clientId("foo-bar")
-                .brokerUrl("tcp://192.168.1.200:1883")
-                .credentials(userAndPassword("kapua-broker", "kapua-password"))
-                .build()) {
-
-            Application.Builder builder=client.buildApplication("app");
-            try {
+    public  void Connect() throws Exception  {
+        try   {
+            final Client client = KuraMqttProfile.newProfile(FuseClient.Builder::new)
+                    .accountName("kapua-sys")
+                    .clientId("mobile_vivek")
+                    .brokerUrl("tcp://192.168.1.200:1883")
+                    .credentials(userAndPassword("kapua-broker", "kapua-password"))
+                    .build();
+            try  {
+                Application.Builder builder=client.buildApplication("Demo");
                 final Application application = builder.build();
                 // wait for connection
-
                 waitForConnection(application.transport());
-
                 // subscribe to a topic
-
-                application.data(Topic.of("my", "topic")).subscribe(message -> {
-                    System.out.format("Received: %s%n", message);
-                });
-
+                application.data(Topic.of("Mobile Data", "Add")).subscribe(
+                        new MessageHandler() {
+                            @Override
+                            public void handleMessage(Payload message) {
+                                System.out.format("Received: %s%n", message);
+                            }
+                        });
+//                        message -> {
+//                    System.out.format("Received: %s%n", message);
+                //});
                 // example payload
-
                 final Payload.Builder payload = new Payload.Builder();
-                payload.put("foo", "bar");
-                payload.put("a", 1);
-
+                payload.put("lat", "5.20");
+                payload.put("long", "7.20");
                 try {
                     // send, handling error ourself
-                    application.data(Topic.of("my", "topic")).send(payload);
+                    application.data(Topic.of("Mobile Data", "Add")).send(payload);
                 } catch (final Exception e) {
                     logger.info("Failed to publish", e);
                 }
-
                 // send, with attached error handler
-
-                application.data(Topic.of("my", "receiver")).subscribe(new MessageHandler() {
-                    @Override
-                    public void handleMessage(Payload payload) {
-                        System.out.format("Received: %s%n",payload );
-                    }
-                });
-
+                application.data(Topic.of("Mobile Data", "Add"))
+                        .errors(
+                                new ErrorHandler<Throwable>() {
+                                    @Override
+                                    public void handleError(Throwable e, Optional<Payload> payload) throws Throwable {
+                                        System.err.println("Failed to publish: " + e.getMessage());
+                                    }
+                                }
+                        )
+                        //handle((e, message) -> System.err.println("Failed to publish: " + e.getMessage())))
+                        .send(payload);
                 // ignoring error
-
-                application.data(Topic.of("my", "topic")).errors(ignore()).send(payload);
-
+                application.data(Topic.of("Mobile Data", "Add")).errors(ignore()).send(payload);
                 // cache sender instance
-
-                final Sender<RuntimeException> sender = application.data(Topic.of("my", "topic")).errors(ignore());
-
+                final Sender<RuntimeException> sender = application.data(Topic.of("Mobile Data", "Add")).errors(ignore());
                 int i = 0;
                 while (i < 10) {
                     // send
-                    sender.send(Payload.of("counter", i++));
+                    //sender.send(Payload.of("counter", i++));
                     Thread.sleep(1_000);
                 }
-
                 // sleep to not run into Paho thread starvation
                 // Thread.sleep(100_000);
+            } catch (Exception throwable) {
+                throwable.printStackTrace();
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
             }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-                e.getMessage();
-            }
-
             Thread.sleep(1_000);
-
         }
         catch (Exception e)
         {
             e.printStackTrace();
-            e.getMessage();
         }
     }
 }
